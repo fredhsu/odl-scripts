c53254fd9bb0828cfee25e24314f01efab7d55da
Fred Hsu <fhsu@cisco.com>
2013-06-11 16:20:39 -0700

    Refactoring

--- python/addflow/odl-addflow.py
+++ python/addflow/odl-addflow.py
@@ -3,11 +3,35 @@ import networkx as nx
 from networkx.readwrite import json_graph
 import httplib2
 
+# If I want to create node objects, but may not need to
+class Node(object):
+  def __init__(self, nodeId, nodeType):
+    self.nodeId = nodeId
+    self.nodeType = nodeType
+
+def node_decoder(obj):
+  if obj['__type__'] == 'Node':
+    return Node(obj['@id'], obj['@type'])
+  return obj
+
 baseUrl = 'http://10.55.17.20:8080/controller/nb/v2'
 containerName = 'default'
 
 h = httplib2.Http(".cache")
 h.add_credentials('admin', 'admin')
+def build_flow_entry(flowName, ingressEdge, egressEdge, node, srcIP, dstIP):
+  # *** Example flow: newFlow = {"installInHw":"false","name":"test2","node":{"@id":"00:00:00:00:00:00:00:07","@type":"OF"},"ingressPort":"1","priority":"500","etherType":"0x800","nwSrc":"10.0.0.7","nwDst":"10.0.0.3","actions":"OUTPUT=2"}
+  etherTypeIP = "0x800"
+  defaultPriority = "500"
+  newFlow = {"installInHw":"false"}
+  ingressPort = ingressEdge['edge']['tailNodeConnector']['@id']
+  egressPort = egressEdge['edge']['headNodeConnector']['@id']
+  switchType = egressEdge['edge']['headNodeConnector']['node']['@type']
+  newFlow.update({"name":flowName})
+  newFlow.update({"node":ingressEdge['edge']['tailNodeConnector']['node']})
+  newFlow.update({"ingressPort":ingressPort, "priority":defaultPriority, "etherType":etherTypeIP, "nwSrc":srcIP, "nwDst":dstIP, "actions":"OUTPUT=" + egressPort})
+  return newFlow
+
 def build_url(baseUrl, service, containerName):
   postUrl = '/'.join([baseUrl, service, containerName])
   print postUrl
@@ -26,11 +50,11 @@ def post_dict(h, url, d):
       body=json.dumps(d),
       )
   return resp, content
+
 # Get all the edges/links
 resp, content = h.request(build_url(baseUrl, 'topology', containerName), "GET")
 edgeProperties = json.loads(content)
 odlEdges = edgeProperties['edgeProperties']
-#print json.dumps(odlEdges, indent=2)
 
 # Get all the nodes/switches
 resp, content = h.request(build_url(baseUrl, 'switch', containerName) + '/nodes/', "GET")
@@ -42,6 +66,8 @@ graph = nx.Graph()
 for node in odlNodes:
   # Might replace this with the actual node object to make stuff easier later
   graph.add_node(node['node']['@id'], nodeType=node['node']['@type'])
+  # this doesn't work b/c dict is not hashable 
+  # graph2.add_node(node['node'])
 for edge in odlEdges:
   e = (edge['edge']['headNodeConnector']['node']['@id'], edge['edge']['tailNodeConnector']['node']['@id'])
   graph.add_edge(*e)
@@ -50,29 +76,18 @@ for edge in odlEdges:
 print "shortest path from 3 to 7" 
 shortest_path = nx.shortest_path(graph, "00:00:00:00:00:00:00:03", "00:00:00:00:00:00:00:07")
 print shortest_path
-newFlow = {}
 srcIP = "10.0.0.7"
 dstIP = "10.0.0.3"
-etherTypeIP = "0x800"
-defaultPriority = "500"
 # Iterate for the 'intermediate' switches, not connected to a host port
 for i, node in enumerate(shortest_path[1:-1]):
-  newflow = {}
   flowName = "test" + str(i)
   ingressEdge = next(edge for edge in odlEdges if edge['edge']['headNodeConnector']['node']['@id'] == shortest_path[i] and edge['edge']['tailNodeConnector']['node']['@id'] == node)
   egressEdge = next(edge for edge in odlEdges if edge['edge']['headNodeConnector']['node']['@id'] == node and edge['edge']['tailNodeConnector']['node']['@id'] == shortest_path[i+2])
-
-  ingressPort = ingressEdge['edge']['tailNodeConnector']['@id']
-  egressPort = egressEdge['edge']['headNodeConnector']['@id']
-  switchType = egressEdge['edge']['headNodeConnector']['node']['@type']
-  switchId = node
-  newFlow.update({"installInHw":"false", "name":flowName})
-  newFlow.update({"node":ingressEdge['edge']['tailNodeConnector']['node']})
-  newFlow.update({"ingressPort":ingressPort, "priority":defaultPriority, "etherType":etherTypeIP, "nwSrc":srcIP, "nwDst":dstIP, "actions":"OUTPUT=" + egressPort})
-#newFlow = {"installInHw":"false","name":"test2","node":{"@id":"00:00:00:00:00:00:00:07","@type":"OF"},"ingressPort":"1","priority":"500","etherType":"0x800","nwSrc":"10.0.0.7","nwDst":"10.0.0.3","actions":"OUTPUT=2"}
   print "*** dump new flow***"
-  print newFlow
-  #print json.dumps(newFlow)
-  postUrl = build_flow_url(baseUrl, 'default', switchType, switchId, flowName)
-  resp, content = post_dict(h, postUrl, newFlow)
+  newFlow = build_flow_entry(flowName, ingressEdge, egressEdge, node, srcIP, dstIP)
+  print json.dumps(newFlow)
+  switchType = newFlow['node']['@type']
+  postUrl = build_flow_url(baseUrl, 'default', switchType, node, flowName)
+  # post the flow to the controller
+  #resp, content = post_dict(h, postUrl, newFlow)
 
