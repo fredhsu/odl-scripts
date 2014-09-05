a3d28fefab8f726b0f39a2451177cb6ecbbecc1e
Rakesh Kumar <gopchandani@gmail.com>
2014-09-05 16:07:59 -0700

    Adding hosts to the topo.

--- python/topo/odl-topo.py
+++ python/topo/odl-topo.py
@@ -13,28 +13,38 @@ h.add_credentials('admin', 'admin')
 resp, content = h.request(baseUrl + 'topology/' + containerName, "GET")
 edgeProperties = json.loads(content)
 odlEdges = edgeProperties['edgeProperties']
-print json.dumps(odlEdges, indent = 2)
 
 # Get all the nodes/switches
 resp, content = h.request(baseUrl + 'switchmanager/' + containerName + 'nodes/', "GET")
 nodeProperties = json.loads(content)
 odlNodes = nodeProperties['nodeProperties']
-print json.dumps(odlNodes, indent = 2)
 
-# Put nodes and edges into a graph
+# Get all the actie hosts
+resp, content = h.request(baseUrl + 'hosttracker/' + containerName + 'hosts/active', "GET")
+hostProperties = json.loads(content)
+hosts = hostProperties["hostConfig"]
+
+# Initialize the graph
 graph = nx.Graph()
+
+#  Put switches in the graph
 for node in odlNodes:
   graph.add_node(node['node']['id'])
+
+#  Put all the edges between switches
 for edge in odlEdges:
   e = (edge['edge']['headNodeConnector']['node']['id'], edge['edge']['tailNodeConnector']['node']['id'])
   graph.add_edge(*e)
-# Print out graph info as a sanity check
-print graph.number_of_nodes()
-print graph.nodes()
-#print json.dumps(odlNodes, indent = 2)
-# These JSON dumps were handy when trying to parse the responses 
-#print json.dumps(topo[0], indent = 2)
 
+#  Put hosts in the graph and the relevant edges
+for host in hosts:
+  graph.add_node(host['networkAddress'])
+  e = (host['networkAddress'], host['nodeId'])
+  graph.add_edge(*e)
+
+# Print out graph info as a sanity check
+print "Number of nodes add in the graph:", graph.number_of_nodes()
+print "Nodes are:", graph.nodes()
 
 # write json formatted data to use in visualization
 d = json_graph.node_link_data(graph)
